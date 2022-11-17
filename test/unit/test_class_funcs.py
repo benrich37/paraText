dd7f2cf45222a3d632568e017f4045855fae5714
benrich37 <76569522+benrich37@users.noreply.github.com>
2022-11-16 19:00:29 -0700

    got one (1) simulated event test working

--- test/unit/test_class_funcs.py
+++ test/unit/test_class_funcs.py
@@ -6,6 +6,7 @@ import unittest
 mainpath = os.path.join(os.path.dirname(__file__), '../../')
 sys.path.append(mainpath)
 from classes import paraText
+from libs import utils
 
 # Follow this https://stackoverflow.com/questions/4083796/how-do-i-run-unittest-on-a-tkinter-app
 
@@ -36,11 +37,29 @@ class TestClassFuncs(TKinterTestCase):
     def test_nothing(self):
         self.assertTrue(True)
 
-    def test_parent_rep_id(self):
+    def test_get_parent_rep_tag(self):
         pattern = "test"
         expected = self.ex.repFlag + pattern
-        output = self.ex.parent_rep_id(pattern)
+        output = self.ex.get_parent_rep_tag(pattern)
         self.assertEqual(expected, output)
+
+    def test_get_child_rep_tag(self):
+        idx = 1
+        sync = True
+        pattern = self.synonyms[0]
+        expected = self.ex.repFlag + str(idx) + self.ex.repIdFlag + str(sync) + self.ex.syncFlag + pattern
+        self.assertEqual(expected, self.ex.get_child_rep_tag(idx, sync, pattern))
+        self.assertEqual(expected, self.ex.get_child_rep_tag(str(idx), str(sync), pattern))
+
+    def test_parse_child_rep_tag(self):
+        idx = 1
+        sync = True
+        pattern = self.synonyms[0]
+        child_rep_tag = self.ex.get_child_rep_tag(idx, sync, pattern)
+        output = self.ex.parse_child_rep_tag(child_rep_tag)
+        self.assertEqual(output[0], idx)
+        self.assertEqual(output[1], str(sync))
+        self.assertEqual(output[2], pattern)
 
     def test_interp_sync_arg(self):
         self.assertEqual(self.ex.interp_sync_arg(None), self.ex.default_sync)
@@ -49,7 +68,7 @@ class TestClassFuncs(TKinterTestCase):
 
     def test_append_options(self):
         pattern = self.synonyms[0]
-        parent_tag = self.ex.parent_rep_id(pattern)
+        parent_tag = self.ex.get_parent_rep_tag(pattern)
         opt_list = self.synonyms
         # Should raise key error before option list is added to the system
         self.assertRaises(KeyError, lambda: self.ex.replace_tags[parent_tag])
@@ -64,8 +83,63 @@ class TestClassFuncs(TKinterTestCase):
         for o in opt_list:
             self.assertEqual(self.ex.replace_tags[parent_tag].count(o), 1)
 
+    def test_get_init_rep_id(self):
+        pattern = self.synonyms[0]
+        parent_tag = self.ex.get_parent_rep_tag(pattern)
+        opt_list = self.synonyms
+        self.assertEqual(0, self.ex.get_init_rep_id(pattern))
+        self.ex.add_tag_rep(pattern, opt_list)
+        self.assertEqual(len(self.ex.rep_replace_tags[parent_tag]) , self.ex.get_init_rep_id(pattern))
 
-    # def test_get_init_rep_id(self):
+    def test_get_last_rep_id(self):
+        pattern = self.synonyms[0]
+        parent_tag = self.ex.get_parent_rep_tag(pattern)
+        opt_list = self.synonyms
+        self.assertEqual(0, self.ex.get_init_rep_id(pattern))
+        self.ex.add_tag_rep(pattern, opt_list)
+        self.assertEqual(len(self.ex.rep_replace_tags[parent_tag]) - 1, self.ex.get_last_rep_id(pattern))
+
+
+    def test_setup_rep_bind_tag_attacker(self):
+        #### SETUP ####
+
+        pattern = self.synonyms[0]
+        opt_list = self.synonyms
+        parent_tag = self.ex.get_parent_rep_tag(pattern)
+        synctag = "True"
+        self.ex.append_options(parent_tag, opt_list)
+        matches = utils.return_matches(self.ex, pattern)
+        for i in range(len(matches)):
+            ctag_i = self.ex.get_child_rep_tag(0 + i,
+                                            synctag,
+                                            pattern)
+            self.ex.append_child_tags(parent_tag, ctag_i)
+            bound1 = matches[i]
+            bound2 = utils.add_to_char_idx(matches[i], len(pattern))
+            self.ex.tag_add(ctag_i,
+                         bound1,
+                         bound2
+                         )
+        attacker_tags = self.ex.rep_replace_tags[parent_tag]
+        attacker_tag = attacker_tags[0]
+        ####
+        # Make sure the most recently added widget is the paraText widget itself
+        self.assertEqual(self.root.winfo_children()[-1].winfo_class(), self.ex.winfo_class())
+        self.ex.setup_rep_bind_tag_attacker(attacker_tag, attacker_tag, parent_tag)
+        self.ex.update_idletasks()
+        # get coordinates of where this was created
+        bounds = self.ex.tag_ranges(attacker_tag)
+        coords = self.ex.bbox(bounds[0])
+        # ESSENTIAL!!! There must be motion event on text with bound events before they can trigger
+        self.ex.event_generate('<Motion>', x=coords[0], y=coords[1])
+        self.ex.event_generate('<Button-2>', x=coords[0], y=coords[1])
+        self.ex.event_generate('<ButtonRelease-2>', x=coords[0], y=coords[1])
+        option_list = self.root.winfo_children()[1]
+        # Most recently created widget should be a TFrame
+        self.assertEqual(option_list.winfo_class(), 'TFrame')
+
+
+
 
 
     def test_Add_Tag_Rep(self):
