703d897427b8a6a1ee9e4f0eebc2a0bcc40ae797
sashiw <sashi.weerawarana@colorado.edu>
2022-11-20 08:23:16 -0700

    tests for char_idx_to_ints

--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -1,1 +1,26 @@
-# TODO
+import unittest
+import os
+import sys
+sys.path.append("../../libs/")
+import utils as u
+
+class TestUtils(unittest.TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         # create temporary data structures
+    
+#     @classmethod
+#     def tearDownClass(cls):
+#         # kill temporary data structures
+    
+    def test_char_idx_to_ints(self):
+        idxs = [1.2]
+        idx1, idx2 = u.char_idx_to_ints('1.2')
+        
+        # positive test
+        self.assertEqual(idx1, 1)
+        self.assertEqual(idx2, 2)
+        
+        # negative test
+        self.assertNotEqual(idx1, 4)
+        self.assertNotEqual(idx2, 5)
