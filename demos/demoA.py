029d2767590005d2e816c3744a6bcb0c3f0d83a5
benrich37 <76569522+benrich37@users.noreply.github.com>
2022-12-08 14:15:34 -0700

    made prettier

--- demos/demoA.py
+++ demos/demoA.py
@@ -6,17 +6,18 @@ sys.path.append(mainpath)
 from classes import paraText
 
 root = tk.Tk()
-root.geometry("440x100")
-root.title('testA')
+root.geometry("600x350")
+root.title('Demo A')
 synonyms = ['concise', 'terse']
 rephrasings = ["However I want to be super duper wordy.",
                "Except I love the sound of my own voice.",
                "But I gotta say what I gotta say."]
 
-myText = paraText.paraText()
-myText.insert('1.0', 'I will be concise. I want to be concise.'
-                     'I hope to one day be concise.' + rephrasings[0])
-myText.grid(column=0, row=0, padx=5, pady=5)
+myText = paraText.paraText(wrap=tk.WORD, font=("Arial", 25), width=40,
+                           border=0)
+myText.insert('1.0', ' I will be concise. I want to be concise.'
+                     ' I hope to one day be concise. ' + rephrasings[0])
+myText.grid(column=0, row=0, padx=24, pady=40)
 
 myText.add_tag_rep(synonyms[0], synonyms, sync=myText.syncTrue)
 myText.add_tag_rep(rephrasings[0], rephrasings, sync=myText.syncFalse)
